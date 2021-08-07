23a3ecd1d6615858a70af0374f4ab61a641e96d0
prajwal <sharmaprajwal11@gmail.com>
2021-08-07 12:13:05 -0700

    Learning Array

---
+++ src/revision/R_4_Array.java
@@ -0,0 +1,12 @@
+package revision;
+
+import java.util.Arrays;
+
+public class R_4_Array {
+    public static void main(String[] args) {
+        int[] marks = {5, 3, 2, 1, 4};
+        for (int i=0;i< marks.length;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
